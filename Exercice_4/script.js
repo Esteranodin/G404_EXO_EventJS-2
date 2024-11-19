285624b345ba034748b727948125ff9ba8bc8ccc
esteranodin <esteranodin@hotmail.fr>
2024-11-19 14:25:16 +0100

    DONE : EXO 4

---
+++ Exercice_4/script.js
@@ -0,0 +1,23 @@
+const buttonVerif = document.querySelector('form button');
+
+buttonVerif.addEventListener('click', handleCompare);
+
+function handleCompare(){
+  const inputPassWord = document.querySelector('#password');
+  const inputConfirm = document.querySelector('#confirmPassword')  
+  
+  if (inputPassWord.value === inputConfirm.value){
+    inputPassWord.classList.add('borderGreen');
+    inputConfirm.classList.add('borderGreen');   
+    inputPassWord.classList.remove('borderRed');
+    inputConfirm.classList.remove('borderRed');
+  }
+
+  else {
+    inputPassWord.classList.add('borderRed');
+    inputConfirm.classList.add('borderRed'); 
+    inputPassWord.classList.remove('borderGreen');
+    inputConfirm.classList.remove('borderGreen');
+  };
+}
+
