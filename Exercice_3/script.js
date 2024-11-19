0babd249bbe18fe3f6e7b4a57073133772340c45
esteranodin <esteranodin@hotmail.fr>
2024-11-19 13:41:17 +0100

    DONE : EXO 3

---
+++ Exercice_3/script.js
@@ -0,0 +1,23 @@
+const textGreen = document.querySelector('#carreVert');
+const textRed = document.querySelector('#carreRouge');
+const textBlue = document.querySelector('#carreBleu');
+const text = document.querySelector('#text');
+
+
+textGreen.addEventListener('click', handleColorGreen);
+function handleColorGreen() {
+    text.classList.remove('textred', 'textblue');
+    text.classList.add('textgreen');
+};
+
+textRed.addEventListener('click', handleColorRed);
+function handleColorRed() {
+    text.classList.remove('textgreen', 'textblue');
+    text.classList.add('textred');
+};
+
+textBlue.addEventListener('click', handleColorBlue);
+function handleColorBlue() {
+    text.classList.remove('textred', 'textgreen');
+    text.classList.add('textblue');
+};
